820588285aea3cd7933e819f976c7904c1e5db9b
mlamb <mlamb128@gmail.com>
2018-12-09 15:07:59 -0800

    Add mock routes to jest Auth.test.js

--- client/src/components/auth/Auth.test.js
+++ client/src/components/auth/Auth.test.js
@@ -2,23 +2,34 @@ import React from 'react';
 import Auth from './Auth';
 import { shallow } from 'enzyme';
 
+
+jest.mock('../../routes/routes.js', () => ({
+  ROUTES: {
+    SIGNUP: {
+      linkTo: () => '/'
+    },
+    SIGNIN: {
+      linkTo: () => '/'
+    }
+  }
+}));
+
 describe('Auth component', () => {
-  const updateLoginType = jest.fn();
 
   it('matches snapshot', () => {
-    const wrapper = shallow(<Auth updateLoginType={updateLoginType} loginType='Sign Up' />);
+    const wrapper = shallow(<Auth loginType='Sign Up' />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('shows appropriate questions for new user', () => {
-    const wrapper = shallow(<Auth updateLoginType={updateLoginType} loginType='Sign Up'/>);
+    const wrapper = shallow(<Auth loginType='Sign Up'/>);
     expect(wrapper.html()).toContain('Are You a Nanny or a Family?');
     expect(wrapper.html()).toContain('Already Have an Account?');
     expect(wrapper.html()).not.toContain('New to Nanny Now?');
   });
 
   it('shows appropriate questions for existing user', () => {
-    const wrapper = shallow(<Auth updateLoginType={updateLoginType} loginType='Sign In'/>);
+    const wrapper = shallow(<Auth loginType='Sign In'/>);
     expect(wrapper.html()).not.toContain('Already Have an Account?');
     expect(wrapper.html()).not.toContain('Are You a Nanny or a Family?');
     expect(wrapper.html()).toContain('New to Nanny Now?');

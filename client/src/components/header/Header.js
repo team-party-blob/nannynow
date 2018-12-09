447f06322431e0624fa97265ee081a0d6edcef04
mlamb <mlamb128@gmail.com>
2018-12-08 17:00:20 -0800

    Add header buttons, hamburger button, media-queries and animation for mobile header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ROUTES from '../../routes/routes';
+import styles from './Header.css';
+import logo from '../../assets/palm_logo.png';
 
 export default function Header() {
   return (
     <header>
-      <nav>
-        <Link to={ROUTES.HOME.linkTo()}>Home</Link>
+      <img src={logo} alt="palm logo"/>
+
+      <label id={styles.hamburgerLabel}>This is the hamburger input.</label>
+      <input type="checkbox" id={styles.hamburgerInput} />
+      <span id={styles.hamburgerSpan}></span>
+
+      <nav id={styles.nav}>
+        <Link className={styles.link} to={ROUTES.HOME.linkTo()}>Dashboard</Link>
+        <Link className={styles.link} to={ROUTES.HOME.linkTo()}>Edit Profile</Link>
+        <Link className={styles.link} to={ROUTES.HOME.linkTo()}>Log In</Link>
+        <Link className={styles.link} to={ROUTES.HOME.linkTo()}>Log Out</Link>
       </nav>
     </header>
   );

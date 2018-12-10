e1b53f42e0f502ef8655c984be69bddedf4e0c0b
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-09 17:47:23 -0800

    fixed jest error (again), 2nd agency test red

--- server/src/tests/e2e/agencies.test.js
+++ server/src/tests/e2e/agencies.test.js
@@ -4,35 +4,6 @@ import app from '../../routes/app';
 const { getAgencies, agenciesSeedData } = require('./helpers/seedData');
 
 describe('agencies routes', () => {
-  it('creates a new agency', () => {
-    // const createdAgencies = getAgencies();
-    // const agencies = agenciesSeedData();
-    const agency = {
-      businessName: 'NWNannies LLC',
-      contactName: 'Linda',
-      streetAddress1: '3 Monroe Parkway Suite P#129',
-      city: 'Lake Oswego',
-      state: 'OR',
-      zip: 97035,
-      phone: 5032455288,
-      businessEmail: 'info@nwnannies.net',
-      website: 'https://nwnannies.net/',
-      hourlyFee: 3.5
-    };
-
-    return request(app)
-      .post('/api/agencies')
-      .send(agency)
-      .then(res => {
-        expect(res.body).toEqual({
-          ...agency,
-          _id: expect.any(String),
-          __v: 0,
-          createdDate: expect.anything()
-        });
-      });
-  });
-
   it('creates an agency with seed data helper', () => {
     const createdAgencies = getAgencies();
     const agencies = agenciesSeedData();
@@ -43,5 +14,16 @@ describe('agencies routes', () => {
       __v: 0,
       createdDate: expect.anything()
     });
+  });
+
+  it('gets a list of all agencies', () => {
+    const createdAgencies = getAgencies();
+
+    return request(app)
+      .get('/api/agencies')
+      .then(res => {
+        expect(res.body.length).toEqual(1);
+        expect(res.body).toContainEqual(createdAgencies[0]);
+      });
   });
 });

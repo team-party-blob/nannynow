ba73a2e89ccc0468830dbe92e715fc3c45b3bfc3
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-12-10 12:59:59 -0800

    first nanny test written and failing

---
+++ server/src/tests/e2e/nannies.test.js
@@ -0,0 +1,18 @@
+import './helpers/db';
+import request from 'supertest';
+import app from '../../routes/app';
+import { getNannies, nanniesSeedData } from './helpers/seedData';
+
+describe.only('nanny routes', () => {
+  it('creates an nanny with seed data helper', () => {
+    const createdNannies = getNannies();
+    const nannies = nanniesSeedData();
+
+    expect(createdNannies[0]).toEqual({
+      ...nannies[0],
+      _id: expect.any(String),
+      __v: 0,
+      createdDate: expect.anything()
+    });
+  });
+});

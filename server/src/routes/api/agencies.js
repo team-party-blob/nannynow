cc5844a0cd0a0d7856f23b610d36962fb1e6106c
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-09 17:50:03 -0800

    agency 2nd test green

--- server/src/routes/api/agencies.js
+++ server/src/routes/api/agencies.js
@@ -1,35 +1,41 @@
 import { Router } from 'express';
 import Agency from '../../models/Agency';
 
-export default Router().post('/', (req, res, next) => {
-  console.log('request at route', req.body);
-  const {
-    businessName,
-    contactName,
-    streetAddress1,
-    streetAddress2,
-    city,
-    state,
-    zip,
-    phone,
-    businessEmail,
-    website,
-    hourlyFee
-  } = req.body;
+export default Router()
+  .post('/', (req, res, next) => {
+    const {
+      businessName,
+      contactName,
+      streetAddress1,
+      streetAddress2,
+      city,
+      state,
+      zip,
+      phone,
+      businessEmail,
+      website,
+      hourlyFee
+    } = req.body;
 
-  Agency.create({
-    businessName,
-    contactName,
-    streetAddress1,
-    streetAddress2,
-    city,
-    state,
-    zip,
-    phone,
-    businessEmail,
-    website,
-    hourlyFee
+    Agency.create({
+      businessName,
+      contactName,
+      streetAddress1,
+      streetAddress2,
+      city,
+      state,
+      zip,
+      phone,
+      businessEmail,
+      website,
+      hourlyFee
+    })
+      .then(agency => res.json(agency))
+      .catch(next);
   })
-    .then(agency => res.json(agency))
-    .catch(next);
-});
+  .get('/', (req, res, next) => {
+    Agency.find()
+      .lean()
+      .then(agencys => res.json(agencys))
+      .catch(next);
+  });

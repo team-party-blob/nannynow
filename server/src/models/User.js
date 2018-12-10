ce097fc86dbb9975785fad97c9433b04a61c52e7
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-09 18:29:27 -0800

    users model done

---
+++ server/src/models/User.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { tokenize, untokenize, hash, compare } from '../utils/auth';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true
+    },
+    role: {
+      type: String,
+      required: [true, 'Role must be admin, nanny, or family'],
+      enum: ['admin', 'nanny', 'family']
+    },
+    createdDate: {
+      type: Date,
+      default: Date.now()
+    },
+    agency: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Agency',
+      required: true
+    },
+    passwordHash: String
+  },
+  {
+    toJSON: {
+      transform: function(doc, ret) {
+        delete ret.__v;
+      }
+    }
+  }
+);
+
+userSchema.virtual('password').set(function(password) {
+  this._tempPassword = password;
+});
+
+userSchema.pre('save', function(next) {
+  this.passwordHash = hash(this._tempPassword);
+  next();
+});
+
+userSchema.methods.compare = function(password) {
+  return compare(password, this.passwordHash);
+};
+
+userSchema.methods.authToken = function() {
+  const jsonUser = this.toJSON();
+  return tokenize(jsonUser);
+};
+
+userSchema.statics.findByToken = function(token) {
+  try {
+    const user = untokenize(token);
+    return Promise.resolve(user);
+  } catch(e) {
+    return Promise.resolve(null);
+  }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

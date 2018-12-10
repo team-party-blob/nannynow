32ba371656fd520e165063262574d61af05a2f94
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-10 11:19:30 -0800

    error handler fixed, all tests passing

--- server/src/middleware/error.js
+++ server/src/middleware/error.js
@@ -1,5 +1,6 @@
-const handler = (err, req, res) => {
-  let code = 500;
+/* eslint-disable-next-line */
+export const handler = (err, req, res, next) => {
+  let code = 503;
   let error = 'Internal Server Error';
 
   if(err instanceof HttpError) {
@@ -10,23 +11,18 @@ const handler = (err, req, res) => {
     error = err.message;
   } else if(process.env.NODE_ENV !== 'production') {
     error = err.message;
-    console.log('handler error 1', err);
+    console.log(err);
   } else {
-    console.log('handler error 2', err);
+    console.log(err);
   }
 
   res.status(code).send({ error });
 };
 
-class HttpError extends Error {
+export class HttpError extends Error {
   constructor({ code, message }) {
     super(message);
     this.code = code;
     this.name = 'HttpError';
   }
 }
-
-module.exports = {
-  handler,
-  HttpError
-};
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, 'Email is required'],
-      unique: true
     },
     role: {
       type: String,

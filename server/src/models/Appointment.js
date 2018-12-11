cbf926e1e150d2ef0a5622139c9c3f934032a155
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-11 07:27:19 -0800

    created appointment model

---
+++ server/src/models/Appointment.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const AppointmentSchema = new mongoose.Schema({
+  arrivalTime: {
+    type: Date,
+    required: [true, 'Start date and time are required.']
+  },
+  departureTime: {
+    type: Date,
+    required: [true, 'End date and time are required.']
+  },
+  family: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'FamilyProfile',
+    required: [true, 'Family is required']
+  },
+  agency: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Agency',
+    required: [true, 'Agency is required']
+  },
+  nanny: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'NannyProfile',
+    required: [true, 'At least one  nanny is required']
+  },
+  request: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'NannyProfile',
+    required: [true, 'At least one  nanny is required']
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now()
+  },
+  agencyFeePerHour: {
+    type: Number,
+    required: [true, 'An hourly agency fee is required']
+  },
+  nannyPricePerHour: {
+    type: Number,
+    required: [true, 'An hourly nanny price is required']
+  }
+});
+
+export default mongoose.model('Appointment', AppointmentSchema);

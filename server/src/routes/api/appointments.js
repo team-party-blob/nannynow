60448220bf45b828b48a03344fb138e138ce3458
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-11 12:04:34 -0800

    get all appts test green

--- server/src/routes/api/appointments.js
+++ server/src/routes/api/appointments.js
@@ -1,28 +1,36 @@
 import { Router } from 'express';
 import Appointment from '../../models/Appointment';
 
-export default Router().post('/', (req, res, next) => {
-  const {
-    arrivalTime,
-    departureTime,
-    family,
-    agency,
-    nanny,
-    request,
-    agencyFeePerHour,
-    nannyPricePerHour
-  } = req.body;
+export default Router()
+  .post('/', (req, res, next) => {
+    const {
+      arrivalTime,
+      departureTime,
+      family,
+      agency,
+      nanny,
+      request,
+      agencyFeePerHour,
+      nannyPricePerHour
+    } = req.body;
 
-  Appointment.create({
-    arrivalTime,
-    departureTime,
-    family,
-    agency,
-    nanny,
-    request,
-    agencyFeePerHour,
-    nannyPricePerHour
+    Appointment.create({
+      arrivalTime,
+      departureTime,
+      family,
+      agency,
+      nanny,
+      request,
+      agencyFeePerHour,
+      nannyPricePerHour
+    })
+      .then(appointment => res.json(appointment))
+      .catch(next);
   })
-    .then(appointment => res.json(appointment))
-    .catch(next);
-});
+
+  .get('/', (req, res, next) => {
+    Appointment.find()
+      .lean()
+      .then(request => res.json(request))
+      .catch(next);
+  });

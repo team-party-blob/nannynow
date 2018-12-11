09fc0b22d549ee991c2dad730b851fe3f3ce54bb
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-11 07:01:36 -0800

    request get all passing

--- server/src/routes/api/requests.js
+++ server/src/routes/api/requests.js
@@ -1,30 +1,38 @@
 import { Router } from 'express';
 import RequestedAppointment from '../../models/RequestedAppointment';
 
-export default Router().post('/', (req, res, next) => {
-  const {
-    startDateTime,
-    endDateTime,
-    birthdays,
-    appointmentComments,
-    description,
-    family,
-    agency,
-    requestedNannies,
-    createdDate
-  } = req.body;
+export default Router()
+  .post('/', (req, res, next) => {
+    const {
+      startDateTime,
+      endDateTime,
+      birthdays,
+      appointmentComments,
+      description,
+      family,
+      agency,
+      requestedNannies,
+      createdDate
+    } = req.body;
 
-  RequestedAppointment.create({
-    startDateTime,
-    endDateTime,
-    birthdays,
-    appointmentComments,
-    description,
-    family,
-    agency,
-    requestedNannies,
-    createdDate
+    RequestedAppointment.create({
+      startDateTime,
+      endDateTime,
+      birthdays,
+      appointmentComments,
+      description,
+      family,
+      agency,
+      requestedNannies,
+      createdDate
+    })
+      .then(request => res.json(request))
+      .catch(next);
   })
-    .then(request => res.json(request))
-    .catch(next);
-});
+
+  .get('/', (req, res, next) => {
+    RequestedAppointment.find()
+      .lean()
+      .then(request => res.json(request))
+      .catch(next);
+  });

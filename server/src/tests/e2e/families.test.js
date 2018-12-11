9d337affdfa4e04a4453e1a6bb064d71a9ea8a6f
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-10 16:44:31 -0800

    get families test red

--- server/src/tests/e2e/families.test.js
+++ server/src/tests/e2e/families.test.js
@@ -22,97 +22,19 @@ describe('families routes', () => {
     });
   });
 
-  // it('hashes a family\'s password', () => {
-  //   const createdAgencies = getAgencies();
+  it('gets a list of all families', () => {
+    const createdFamilies = getFamilies();
 
-  //   return Family.create({
-  //     email: 'test@test.com',
-  //     password: 'test',
-  //     agency: createdAgencies[0]._id,
-  //     role: 'family'
-  //   }).then(family => {
-  //     expect(family.passwordHash).not.toEqual('test');
-  //     expect(compare('test', family.passwordHash));
-  //   });
-  // });
-
-  // it('compares passwords', () => {
-  //   const createdAgencies = getAgencies();
-
-  //   const family = {
-  //     email: 'test@test.com',
-  //     password: 'test',
-  //     agency: createdAgencies[0]._id,
-  //     role: 'family'
-  //   };
-
-  //   Family.create(family).then(createdFamily => {
-  //     expect(createdFamily.compare(family.password)).toBeTruthy();
-  //     expect(createdFamily.compare('failing')).toBeFalsy();
-  //   });
-  // });
-
-  // it('signs in a family', () => {
-  //   return request(app)
-  //     .post('/api/families/signin')
-  //     .send({ email: 'admin@test.com', password: '123' })
-  //     .then(res => {
-  //       checkOk(res);
-  //       expect(res.body.token).toEqual(expect.any(String));
-  //     });
-  // });
-
-  // it('rejects a sign in with a bad password', () => {
-  //   return request(app)
-  //     .post('/api/families/signin')
-  //     .send({ email: 'admin@test.com', password: 'badpassword' })
-  //     .then(checkStatus(401));
-  // });
-
-  // it('rejects a sign in with a token but bad password', () => {
-  //   let token;
-  //   return request(app)
-  //     .post('/api/families/signin')
-  //     .send({ email: 'admin@test.com', password: '123' })
-  //     .then(res => {
-  //       token = res.body.token;
-  //     })
-  //     .then(
-  //       request(app)
-  //         .post('/api/families/signin')
-  //         .set('Authorization', `Bearer ${token}`)
-  //         .send({ email: 'nanny@test.com', password: 'badpassword' })
-  //         .then(checkStatus(401))
-  //     );
-  // });
-
-  // it('verifies a signed in family', () => {
-  //   return request(app)
-  //     .post('/api/families/signin')
-  //     .send({ email: 'admin@test.com', password: '123' })
-  //     .then(res => {
-  //       return request(app)
-  //         .get('/api/families/verify')
-  //         .set('Authorization', `Bearer ${res.body.token}`)
-  //         .then(res => {
-  //           expect(res.body).toEqual({ success: true });
-  //         });
-  //     });
-  // });
-
-  // it('gets a list of all families', () => {
-  //   const createdFamilies = getFamilies();
-
-  //   return request(app)
-  //     .get('/api/families')
-  //     .then(res => {
-  //       expect(res.body.length).toEqual(4);
-  //       expect(res.body).toContainEqual(createdFamilies[0]);
-  //       expect(res.body).toContainEqual(createdFamilies[1]);
-  //       expect(res.body).toContainEqual(createdFamilies[2]);
-  //       expect(res.body).toContainEqual(createdFamilies[3]);
-  //     });
-  // });
+    return request(app)
+      .get('/api/families')
+      .then(res => {
+        expect(res.body.length).toEqual(4);
+        expect(res.body).toContainEqual(createdFamilies[0]);
+        expect(res.body).toContainEqual(createdFamilies[1]);
+        expect(res.body).toContainEqual(createdFamilies[2]);
+        expect(res.body).toContainEqual(createdFamilies[3]);
+      });
+  });
 
   // it('gets a family by id', () => {
   //   const createdFamilys = getFamilys();

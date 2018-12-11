4021ec3173ccab02ed09550a74f03244ef772acb
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-10 17:21:46 -0800

    Finished basic CRUD for family profiles (#16)
    * get families test red
    
    * get all families passing
    
    * get family by id test red
    
    * get family by id test passed
    
    * delete family by id test red
    
    * deletes a family by id green
    
    * updates a family by id red
    
    * updates a family by id green

--- server/src/routes/api/families.js
+++ server/src/routes/api/families.js
@@ -1,38 +1,103 @@
 import { Router } from 'express';
 import FamilyProfile from '../../models/FamilyProfile';
 
-export default Router().post('/', (req, res, next) => {
-  const {
-    name,
-    streetAddress1,
-    streetAddress2,
-    city,
-    state,
-    zip,
-    phone,
-    email,
-    user,
-    agency,
-    description,
-    numOfChildren,
-    birthdays
-  } = req.body;
+export default Router()
+  .post('/', (req, res, next) => {
+    const {
+      name,
+      streetAddress1,
+      streetAddress2,
+      city,
+      state,
+      zip,
+      phone,
+      email,
+      user,
+      agency,
+      description,
+      numOfChildren,
+      birthdays
+    } = req.body;
 
-  FamilyProfile.create({
-    name,
-    streetAddress1,
-    streetAddress2,
-    city,
-    state,
-    zip,
-    phone,
-    email,
-    user,
-    agency,
-    description,
-    numOfChildren,
-    birthdays
+    FamilyProfile.create({
+      name,
+      streetAddress1,
+      streetAddress2,
+      city,
+      state,
+      zip,
+      phone,
+      email,
+      user,
+      agency,
+      description,
+      numOfChildren,
+      birthdays
+    })
+      .then(family => res.json(family))
+      .catch(next);
   })
-    .then(family => res.json(family))
-    .catch(next);
-});
+
+  .get('/', (req, res, next) => {
+    FamilyProfile.find()
+      .lean()
+      .then(families => res.json(families))
+      .catch(next);
+  })
+
+  .get('/:id', (req, res, next) => {
+    const { id } = req.params;
+    FamilyProfile.findById(id)
+      .lean()
+      .then(family => res.json(family))
+      .catch(next);
+  })
+
+  .delete('/:id', (req, res, next) => {
+    const { id } = req.params;
+    FamilyProfile.findByIdAndDelete(id)
+      .then(family => res.json({ removed: !!family }))
+      .catch(next);
+  })
+
+  .put('/:id', (req, res, next) => {
+    const { id } = req.params;
+    const {
+      name,
+      streetAddress1,
+      streetAddress2,
+      city,
+      state,
+      zip,
+      phone,
+      email,
+      user,
+      agency,
+      description,
+      numOfChildren,
+      birthdays
+    } = req.body;
+
+    FamilyProfile.findByIdAndUpdate(
+      id,
+      {
+        name,
+        streetAddress1,
+        streetAddress2,
+        city,
+        state,
+        zip,
+        phone,
+        email,
+        user,
+        agency,
+        description,
+        numOfChildren,
+        birthdays
+      },
+      { new: true }
+    )
+      .lean()
+      .then(family => res.json(family))
+      .catch(next);
+  });

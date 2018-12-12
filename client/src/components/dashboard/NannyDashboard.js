a271409b90498156ae803a86aee38d80025e2832
mlamb <mlamb128@gmail.com>
2018-12-12 12:14:42 -0800

    Refactor Dashboard component

--- client/src/components/dashboard/NannyDashboard.js
+++ client/src/components/dashboard/NannyDashboard.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class NannyDashboard extends Component {
   render() {
-    console.log(this.props)
     return (
       <div>
         <h1>I am a Nanny Dashboard!</h1>
       </div>
-    )
+    );
   }
 }

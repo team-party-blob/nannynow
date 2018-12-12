69dd03e68044c6919b579d79fbede9fcbbcfa76c
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-12-12 14:42:03 -0800

    route to back end written

--- client/src/services/profileApi.js
+++ client/src/services/profileApi.js
@@ -2,7 +2,7 @@ import { put, post } from './request';
 
 export const updateProfile = (id, profileInfo) => {
   const isFamily = profileInfo.birthdays;
-  console.log(profileInfo)
+
   if(isFamily) return put(`/api/families/${id}`, profileInfo);
   if(!isFamily) return put(`/api/nannies/${id}`, profileInfo);
 };

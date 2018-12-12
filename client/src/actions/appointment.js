83e3af7f1c95579084e00673ba28e3422ca382cf
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-12 15:27:00 -0800

    get appointments action created

---
+++ client/src/actions/appointment.js
@@ -0,0 +1,13 @@
+import { getAppointments as getAppointmentsService } from '../services/profileApi';
+
+export const GET_APPOINTMENTS = 'GET_APPOINTMENTS';
+export const GET_APPOINTMENTS_LOAD_START = 'GET_APPOINTMENTS_LOAD_START';
+export const GET_APPOINTMENTS_LOAD_END = 'GET_APPOINTMENTS_LOAD_END';
+export const GET_APPOINTMENTS_ERROR = 'GET_APPOINTMENTS_ERROR';
+export const getAppointments = (userId) => ({
+  type: GET_APPOINTMENTS,
+  loadStart: GET_APPOINTMENTS_LOAD_START,
+  loadEnd: GET_APPOINTMENTS_LOAD_END,
+  errorType: GET_APPOINTMENTS_ERROR,
+  payload: getAppointmentsService(userId)
+});

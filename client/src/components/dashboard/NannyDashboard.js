ebc2135db646d7e7c8365e8db3bdffaa67336ce9
Mike <mlamb128@gmail.com>
2018-12-12 14:25:31 -0800

    Family dashboard (#42)
    * Change session test to pass with profile being sent back. Working on signOut
    
    * Fix signUp route
    
    * Profile of existing user rendering on NannyProfile page
    
    * Reducer and action tests written and passing
    
    * Create FamilyProfile component, ProfileContainer, and Profile components which conditionally render depending on whether it's a family or nanny
    
    * Can remove children from families component
    
    * State selector box in FamilyProfile component works
    
    * Put working on FamilyProfile
    
    * add put to nanny profile
    
    * added post new profile to nanny
    
    * added post for family
    
    * Comment out routes in NannyProfile component
    
    * Make most of the profile pages' required input fields required. Still need to figure out family birthdays
    
    * Update snapshot
    
    * Make birthday family date field not required
    
    * Send agency and user info for nanny when creating profile
    
    * Set up DB scaffolding
    
    * Refactor Dashboard component
    
    * Linter error

---
+++ client/src/components/dashboard/NannyDashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NannyDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am a Nanny Dashboard!</h1>
+      </div>
+    );
+  }
+}

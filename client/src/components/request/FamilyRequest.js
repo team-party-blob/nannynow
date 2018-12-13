9ecb0adab272ee89f8593e08210de1e3ae7bcf32
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-12-13 11:14:42 -0800

    render works for nanny schedule

--- client/src/components/request/FamilyRequest.js
+++ client/src/components/request/FamilyRequest.js
@@ -1,15 +1,22 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import FilteredNannies from './FilteredNannies';
+import Range from '../dashboard/Range';
 
 export default class FamilyRequest extends PureComponent {
   static propTypes ={
     profile: PropTypes.object.isRequired,
-    createRequest: PropTypes.func.isRequired
+    createRequest: PropTypes.func.isRequired,
+    filteredNannies: PropTypes.array,
+    searchQuery: PropTypes.object.isRequired,
+    updateSearchQuery: PropTypes.func.isRequired,
+    fetchFilteredNannies: PropTypes.func.isRequired
   };
 
   state = {
     birthdays: [],
     selectedChildren: [],
+    searchQuery: {},
     closed: false,
     startDateTime: '',
     endDateTime: '',
@@ -18,35 +25,39 @@ export default class FamilyRequest extends PureComponent {
   };
 
   componentDidMount() {
-    const { profile } = this.props;
-    this.setState({ birthdays: profile.birthdays });
+    const { profile, fetchFilteredNannies, searchQuery } = this.props;
+    this.setState({ birthdays: profile.birthdays, searchQuery });
+
   }
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
   };
+  handleStartChange = date => {
+    this.setState({ startDateTime: date });
+  };
 
-  handleSubmit = event => {
+  handleEndChange = date => {
+    this.setState({ endDateTime: date });
+  };
+
+  handleUpdateSearchTerm = event => {
     event.preventDefault();
     const {
       birthdays,
-      closed,
       startDateTime,
       endDateTime,
-      appointmentComments,
-      requestedNannies
+      appointmentComments
     } = this.state;
-    const { createRequest } = this.props;
-    const request =
+    const { updateSearchQuery } = this.props;
+    const query =
      {
        birthdays,
-       closed,
        startDateTime,
        endDateTime,
-       appointmentComments,
-       requestedNannies
+       appointmentComments
      };
-    createRequest(request);
+    updateSearchQuery(query);
   };
 
 
@@ -57,7 +68,6 @@ export default class FamilyRequest extends PureComponent {
       startDateTime,
       endDateTime,
       appointmentComments,
-      requestedNannies
     } = this.state;
     const childBirthdays = birthdays.map((birthday, i) => {
       const slicedBirthday = birthday.slice(0, 10);
@@ -73,11 +83,19 @@ export default class FamilyRequest extends PureComponent {
       <Fragment>
         <h1>Request an Appointment</h1>
         <div>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleUpdateSearchTerm}>
             <label htmlFor='birthdays'>Children:</label>
             {childBirthdays}
+            <Range
+              onStartChange={this.handleStartChange}
+              onEndChange={this.handleEndChange}
+              start={startDateTime}
+              end={endDateTime}
+            />
             <label htmlFor='comments'>Appointment Comments:</label><br/>
             <textarea name="comments" value={appointmentComments} onChange={this.handleChange}></textarea>
+          </form>
+          <form onSubmit={this.handleFormSubmit}>
           </form>
           <FilteredNannies filteredNannies={filteredNannies}/>
         </div>
--- client/src/components/request/FilteredNannies.js
+++ client/src/components/request/FilteredNannies.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 export default function FilteredNannies({ FilteredNannies }) {
-
-  const nannyList = FilteredNannies.map((nanny, i) => {
+  const fakeFilteredNannies = [
+    {
+      _id: '1234',
+      name: 'Mrs. Doubtfire',
+      streetAddress1: '3 Monroe Parkway Suite P#129',
+      city: 'Lake Oswego',
+      state: 'OR',
+      zip: '97035',
+      phone: '5105010844',
+      description:
+      'I am a nanny in disguise to spy on children for my own personal gain',
+      age: 42,
+      pricePerHour: 8.25,
+      createdDate: '12-08-83'
+    },
+    {
+      _id: '1234',
+      name: 'Anothernanny',
+      streetAddress1: '3 Monroe Parkway Suite P#129',
+      city: 'Lake Oswego',
+      state: 'OR',
+      zip: '97035',
+      phone: '5105010844',
+      description:
+      'I am a nanny in disguise to spy on children for my own personal gain',
+      age: 42,
+      pricePerHour: 8.25,
+      createdDate: '12-08-83'
+    }
+  ];
+  const nannyList = fakeFilteredNannies.map((nanny, i) => {
     return <li key={i}>{nanny.name}</li>;
   });
   return (

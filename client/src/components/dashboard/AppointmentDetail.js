8d6f89ab1405509ab6322cc3582484f0a35bea15
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-13 10:56:11 -0800

    switching branches, progress on appointment detail render

--- client/src/components/dashboard/AppointmentDetail.js
+++ client/src/components/dashboard/AppointmentDetail.js
@@ -1,6 +1,7 @@
 import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import moment from 'moment';
 
 class AppointmentDetail extends PureComponent {
   static propTypes = {
@@ -20,12 +21,22 @@ class AppointmentDetail extends PureComponent {
 
   render() {
     const { detail } = this.props;
-    console.log('detail', detail)
     if(!detail) return null;
+    const ageComponents = detail.request.birthdays.map((birthday, i) => {
+      const age = moment([birthday]);
+      return <li>Child 1: ${age}<li>
+    })
+
     return (
       <Fragment>
         <h3>Appointment Detail</h3>
-        <p></p>
+        <p>Start Time: {detail.request.startDateTime}</p>
+        <p>End Time: {detail.request.endDateTime}</p>
+        <p>Number of Children: {detail.request.birthdays.length}</p>
+        <p>Ages of Children: {moment([detail.request.birthdays[1]]).fromNow(true)}</p>
+        <ul>Appointment Comments: {detail.request.appointmentComments}
+          {ageComponents}
+        </ul>
       </Fragment>
     );
   }

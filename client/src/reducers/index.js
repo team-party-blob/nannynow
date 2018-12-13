b3d49bc1e4ef5e77811d6666f4588ad5221d7c54
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-12-12 16:18:29 -0800

    reducer added for request

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import session from './session';
+import request from './requests/request';
 
 export default combineReducers({
-  session
+  session,
+  request
 });

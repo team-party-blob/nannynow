f6615bb0cf9f69c173abab3eca016814ce34a9a0
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-13 11:26:32 -0800

    passing: gets a list of all available times for one nanny by user id (#46)

---
+++ client/src/components/dashboard/AppointmentDetail.js
@@ -0,0 +1,33 @@
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+class AppointmentDetail extends PureComponent {
+  static propTypes = {
+    detail: PropTypes.object,
+    fetchAppointment: PropTypes.func.isRequired,
+    match: PropTypes.object.isRequired,
+    fetchNanny: PropTypes.func.isRequired,
+    fetchFamily: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    const { fetchAppointment } = this.props;
+    const { appointmentId, userId } = this.props.match.params;
+
+    fetchAppointment(appointmentId, userId);
+  }
+
+  render() {
+    const { detail } = this.props;
+    if(!detail) return null;
+    return (
+      <Fragment>
+        <h3>Appointment Detail</h3>
+        <p></p>
+      </Fragment>
+    );
+  }
+}
+
+export default withRouter(AppointmentDetail);

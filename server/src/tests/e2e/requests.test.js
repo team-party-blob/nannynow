0b020692c1b8627815f2766fc9e742a2e4d56aa7
mlamb <mlamb128@gmail.com>
2019-02-02 15:27:01 -0800

    Write test for user specific requests

--- server/src/tests/e2e/requests.test.js
+++ server/src/tests/e2e/requests.test.js
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '../../routes/app';
 import {
   getRequestedAppointments,
-  requestedAppointmentsSeedData
+  requestedAppointmentsSeedData,
 } from './helpers/seedData';
 
 describe('requested appointments routes', () => {
@@ -22,18 +22,18 @@ describe('requested appointments routes', () => {
         {
           _id: expect.any(String),
           nanny: requestedAppointments[0].requestedNannies[0].nanny,
-          status: 'no response'
+          status: 'no response',
         },
         {
           _id: expect.any(String),
           nanny: requestedAppointments[0].requestedNannies[1].nanny,
-          status: 'no response'
-        }
+          status: 'no response',
+        },
       ],
       closed: expect.any(Boolean),
       _id: expect.any(String),
       __v: 0,
-      createdDate: expect.anything()
+      createdDate: expect.anything(),
     });
   });
 
@@ -71,7 +71,7 @@ describe('requested appointments routes', () => {
     return request(app)
       .put(`/api/requests/${createdRequestedAppointments[0]._id}`)
       .send({
-        appointmentComments: 'Test'
+        appointmentComments: 'Test',
       })
       .then(res => {
         expect(res.body.appointmentComments).toEqual('Test');
@@ -94,7 +94,7 @@ describe('requested appointments routes', () => {
     const { requestedNannies } = createdRequestedAppointments[0];
     const statusUpdate = {
       nannyId: requestedNannies[0].nanny,
-      status: 'accepted'
+      status: 'accepted',
     };
 
     return request(app)
@@ -102,7 +102,66 @@ describe('requested appointments routes', () => {
       .send(statusUpdate)
       .then(res => {
         expect(res.body.nModified).toEqual(1);
+        return request(app)
+          .get(`/api/requests/${requestId}`)
+          .then(res => {
+            expect(res.body.requestedNannies).toEqual(
+              expect.arrayContaining([
+                expect.objectContaining({ status: 'accepted' }),
+              ]),
+            );
+          });
       });
+  });
 
+  it('fetches requestedAppointments for a specific nanny', () => {
+    const createdRequestedAppointments = getRequestedAppointments();
+    const nannyId = createdRequestedAppointments[0].requestedNannies[0].nanny;
+    return request(app)
+      .get(`/api/requests/user/${nannyId}`)
+      .then(res => {
+        expect(res.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              requestedNannies: expect.arrayContaining([
+                expect.objectContaining({ nanny: nannyId }),
+              ]),
+            }),
+          ]),
+        );
+      });
+  });
+
+  it('only fetches requests that the nanny has not declined', () => {
+    const createdRequestedAppointments = getRequestedAppointments();
+    const requestId = createdRequestedAppointments[0]._id;
+    const { requestedNannies } = createdRequestedAppointments[0];
+    const statusUpdate = {
+      nannyId: requestedNannies[0].nanny,
+      status: 'declined',
+    };
+
+    return request(app)
+      .patch(`/api/requests/status/${requestId}`)
+      .send(statusUpdate)
+      .then(res => {
+        expect(res.body.nModified).toEqual(1);
+        return request(app)
+          .get(`/api/requests/user/${requestedNannies[0].nanny}`)
+          .then(res => {
+            expect(res.body).not.toEqual(
+              expect.arrayContaining([
+                expect.objectContaining({
+                  requestedNannies: expect.arrayContaining([
+                    expect.objectContaining({ status: 'declined' }),
+                  ]),
+                }),
+              ]),
+            );
+          });
+      });
+    // createdRequestedAppointments[0].requestedNannies[0].status = 'reject';
+    // const rejectedRequest = createdRequestedAppointments[0];
+    // const rejectingNanny = rejectedRequest.requestedNannies[0].nanny;
   });
 });
